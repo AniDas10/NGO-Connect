72605cbf6bee770f2f56caf5a0e84d7161affaa1
Aniket Shahane <mr_robot@mr.robot-X510UQ>
2020-02-02 00:26:47 +0530

    Event and NGO Recommendations working

--- Connect/Connect/urls.py
+++ Connect/Connect/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from NGO import views
+from Event import views as event_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('show/<str:title>', views.recommend_ngo)
+    path('show/<str:title>', views.recommend_ngo),
+    path('event_rec/<str:event>', event_views.recommend_event)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Connect/Event/views.py
+++ Connect/Event/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Event
+from .events_rec import rec_events
+import pandas as pd
+from django.http import HttpResponse
 
-# Create your views here.
+def recommend_event(request, event):
+    data = Event.objects.all()
+    events_data = []
+    for d in data:
+        events_data.append({'Name': d.name, 'Purpose': d.purpose})
+    recs = rec_events(pd.DataFrame(events_data), event)
+    payload = {
+        'Events': data,
+        'Recommendations': recs
+    }
+    return render(request, 'aspiration/events.html', payload)
