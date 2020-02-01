ea865d61eae9f479c9a4e5ca773c4b7953cee20a
Aniket Shahane <mr_robot@mr.robot-X510UQ>
2020-02-01 19:31:36 +0530

    Add the ngo form

--- Connect/Citizen/models.py
+++ Connect/Citizen/models.py
@@ -16,3 +16,4 @@ class Citizen(models.Model):
     follows = models.ManyToManyField(NGO)
     points = models.IntegerField(default=10)
     city = models.CharField(max_length=240)
+    email = models.EmailField()
